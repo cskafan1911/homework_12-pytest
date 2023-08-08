b07dede43757eac85e4a5914e235dd6f0e2e87b8
Dmitry Dronov <cskafan1911@gmail.com>
2023-08-08 20:16:21 +0300

    fixed bugs in the test

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,9 +1,15 @@
 from utils import arrs
+import pytest
 
 
-def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 3
-    assert arrs.get([], 0, "test") == "test"
+def test_get_success():
+    assert arrs.get([1, 2, 3], 1, "test") == 2
+    assert arrs.get([1, 2, 3], -1, "test") == "test"
+
+
+def test_get__empty_list():
+    with pytest.raises(IndexError):
+        arrs.get([], 0)
 
 
 def test_slice():
